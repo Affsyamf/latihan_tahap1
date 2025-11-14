dd1c3c377e9c5245d4a93916327163b3b7a68351
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-14 10:31:52 +0700

    perbaikan create py mengimport sistem class dari tugas.oy

--- create.py
+++ create.py
@@ -1,16 +1,14 @@
 import database
 from read import read
+from tugas import Tugas
 
 def create():
     print("=== Tambah Tugas ===")
     judul_tugas = input("Masukan Judul Tugas : ")
     deskripsi_tugas = input("Masukan Deskripsi : ")
     diberikan_kepada = input("Masukan Nama : ")
-    tugas = {
-        "judul": judul_tugas,
-        "desc": deskripsi_tugas,
-        "diberikan": diberikan_kepada
-    }
     
-    database.list_tugas.append(tugas)
+    tugas_baru = Tugas(judul = judul_tugas, nama = diberikan_kepada, deksripsi=deskripsi_tugas)
+    
+    database.list_tugas.append(tugas_baru)
     read()
